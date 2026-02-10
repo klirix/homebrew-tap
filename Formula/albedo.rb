bc7396be4443a09775f556cced9806ca04c26cdd
Askhat Saiapov <askhat.saiapov@gmail.com>
2026-02-10 03:08:28 +0100

    version albedo

--- Formula/albedo.rb
+++ Formula/albedo.rb
@@ -7,11 +7,11 @@ class Albedo < Formula
   # `klirix/homebrew-tap` or `klirix/homebrew-albedo`).
   #
   # Recommended for stable releases:
-  #   url "https://github.com/klirix/albedo/archive/refs/tags/vX.Y.Z.tar.gz"
-  #   sha256 "<fill-me-in>"
+  url "https://github.com/klirix/albedo/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "9635f9cde7bf9e05c92aac1ef469f0672edc2101"
   #
   # Until you cut a tagged release, install from git:
-  head "https://github.com/klirix/albedo.git", branch: "main"
+  # head "https://github.com/klirix/albedo.git", branch: "main"
 
   depends_on "zig" => :build
 
